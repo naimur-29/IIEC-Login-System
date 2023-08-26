fa8095d0c2c58296bc6b8d7d871f3d26186f9c3c
Naim <skylike2904@gmail.com>
2023-08-26 23:22:15 +0600

    created client using vite->react.js, using SASS, set up TODO.md

---
+++ TODO.md
@@ -0,0 +1,6 @@
+### 26 Aug 2023
+
+- set up node, express
+- set up mongoDB
+- build simple post, get, update, delete server
+- set up authentication
---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./scss/app.scss";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
